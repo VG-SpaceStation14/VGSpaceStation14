78856c6c1e6f0afc9240a1901ca3ecf5331a8390
Albuka <b.v.v03092013@gmail.com>
2026-05-17 14:35:49 +0300

    [TWEAK] явный null нашей строчке в бд

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -440,7 +440,7 @@ namespace Content.Server.Database
         public string OOCNotes { get; set; } = null!;
         public string HeadshotUrl { get; set; } = null!;
         //ADT-tweak-end
-        public string? PdaWallpaperPath { get; set; } // VG-Tweak
+        public string? PdaWallpaperPath { get; set; } = null!; // VG-Tweak
         public int Age { get; set; }
         public string Sex { get; set; } = null!;
         public string Gender { get; set; } = null!;
